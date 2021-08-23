7355f465d72b24cf4f5bb61a758dee6cc4880957
Miguel Oliveira <miguel.oliveira4224@gmail.com>
2021-08-23 16:22:56 -0300

    Clean up miner code

--- src/miner.rs
+++ src/miner.rs
@@ -7,15 +7,19 @@ const THREAD_ITER: usize = 4096;
 const DESIRED_ITER_TIME: f64 = 1_f64;
 const KERNEL_SRC: &str = include_str!("kernel.cl");
 
+macro_rules! res {
+    ($expr:expr) => {
+        $expr.map_err(|e| anyhow::anyhow!(e))
+    }
+}
+
 pub struct Miner {
     id: usize,
     pq_ocl: ProQue,
     kernel: Kernel,
     solved_buffer: Buffer<u8>,
     pkey_buffer: Buffer<u8>,
-    nonce: u64,
-    local_size: usize,
-    work_size: usize,
+    local_size: f64,
     tx: Sender<(usize, Option<Vec<u8>>, f64)>
 }
 
@@ -28,11 +32,10 @@ impl Miner {
         tx: Sender<(usize, Option<Vec<u8>>, f64)>
     ) -> anyhow::Result<Self> {
         // Get local work size from device
-        let local_size = device.device.info(DeviceInfo::MaxWorkGroupSize)
-            .map_err(|e| anyhow!(e))?;
+        let local_size = res!(device.device.info(DeviceInfo::MaxWorkGroupSize))?;
         let local_size = match local_size {
-            DeviceInfoResult::MaxWorkGroupSize(local_size) => local_size,
-            _ => 0_usize
+            DeviceInfoResult::MaxWorkGroupSize(local_size) => local_size as f64,
+            _ => 0.0,
         };
 
         // Build ProQue
@@ -56,7 +59,7 @@ impl Miner {
             .queue(pq_ocl.queue().clone())
             .flags(MemFlags::new().read_only())
             .len(trie.len())
-            .copy_host_slice(&trie)
+            .copy_host_slice(trie)
             .build()
             .map_err(|e| anyhow!(e))?;
 
@@ -77,7 +80,7 @@ impl Miner {
 
         // Build kernel
         let kernel = pq_ocl.kernel_builder("mine")
-            .global_work_size(local_size)
+            .global_work_size(local_size as usize)
             .arg_named("entropy", &entropy_buffer)
             .arg_named("trie", &trie_buffer)
             .arg_named("nonce", 0u64)
@@ -94,66 +97,59 @@ impl Miner {
             pkey_buffer,
             local_size,
             tx,
-            nonce: 0_u64,
-            work_size: local_size,
         })
     }
 
+    fn enqueue_kernel(&self) -> anyhow::Result<()> {
+        res!(unsafe { self.kernel.enq() })?;
+        res!(self.pq_ocl.finish())
+    }
+
+    fn get_solved(&self) -> anyhow::Result<bool> {
+        let mut solved = vec![0];
+        res!(self.solved_buffer.read(&mut solved).enq())?;
+        Ok(solved[0] == 1)
+    }
+
+    fn set_solved(&self, solved: bool) -> anyhow::Result<()> {
+        res!(self.solved_buffer.write(&vec![solved as u8]).enq())
+    }
+
+    fn get_pkey(&self) -> anyhow::Result<Vec<u8>> {
+        let mut pkey = vec![0; 32];
+        res!(self.pkey_buffer.read(&mut pkey).enq())?;
+        Ok(pkey)
+    }
+
     pub fn mine(&mut self) -> anyhow::Result<()> {
-        let mut vec_solved = vec![0];
+        let mut nonce = 0u64;
+        let mut work_size = self.local_size;
+        let local_size = self.local_size;
 
         loop {
             let t0 = Instant::now();
 
-            // Enqueue kernel and wait for it
-            unsafe { self.kernel.enq() }.map_err(|e| anyhow!(e))?;
-            self.pq_ocl.finish().map_err(|e| anyhow!(e))?;
+            self.enqueue_kernel()?;
 
-            // Read from solved
-            self.solved_buffer.read(&mut vec_solved).enq()
-                .map_err(|e| anyhow!(e))?;
-
-            // Re-calculate global work size to match the fixed iter time
-            let dt = Instant::now() - t0;
-            let dt_vs_desired_coefficient = DESIRED_ITER_TIME / dt.as_secs_f64();
-            let new_work_size = f64::round(
-                (self.work_size as f64 * dt_vs_desired_coefficient) / self.local_size as f64
-            ) as usize * self.local_size;
-            let new_work_size = std::cmp::max(new_work_size, self.local_size);
-            let old_work_size = self.work_size;
-            if new_work_size != old_work_size {
-                self.kernel.set_default_global_work_size(SpatialDims::One(new_work_size));
-                self.work_size = new_work_size;
-            }
+            // Recalculate global work size to match fixed iter time
+            let dt = (Instant::now() - t0).as_secs_f64();
+            let hash_rate = THREAD_ITER as f64 * work_size / dt;
+            let desired_multiplier = DESIRED_ITER_TIME / dt;
+            work_size *= desired_multiplier;
+            work_size = f64::round(work_size / local_size) * local_size;
+            work_size = f64::max(work_size, self.local_size);
+            self.kernel.set_default_global_work_size(SpatialDims::One(work_size as usize));
 
             // Increment nonce
-            self.nonce += 1;
-            self.kernel.set_arg("nonce", self.nonce)
-                .map_err(|e| anyhow!(e))?;
+            nonce += 1;
+            res!(self.kernel.set_arg("nonce", nonce))?;
 
             // Send information to the main thread
-            if vec_solved[0] == 1 {
-                let mut vec_pkey = vec![0; 32];
-                self.solved_buffer.write(&vec_pkey[..1]).enq()
-                    .map_err(|e| anyhow!(e))?;
-                self.pkey_buffer.read(&mut vec_pkey).enq()
-                    .map_err(|e| anyhow!(e))?;
-
-                let dt = Instant::now() - t0;
-                let hash_rate = THREAD_ITER as f64
-                    * old_work_size as f64
-                    / dt.as_secs_f64();
-
-                self.tx.send((self.id, Some(vec_pkey), hash_rate))
-                    .map_err(|e| anyhow!(e))?;
+            if self.get_solved()? {
+                self.set_solved(false)?;
+                self.tx.send((self.id, Some(self.get_pkey()?), hash_rate))?;
             } else {
-                let dt = Instant::now() - t0;
-                let hash_rate = THREAD_ITER as f64
-                    * old_work_size as f64
-                    / dt.as_secs_f64();
-
-                self.tx.send((self.id, None, hash_rate))
-                    .map_err(|e| anyhow!(e))?;
+                self.tx.send((self.id, None, hash_rate))?;
             }
         }
     }
